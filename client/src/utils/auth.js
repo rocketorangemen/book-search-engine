ffe632987fb98f3eb14da322eff39d3f4ffd70b0
Mon <rocketorangemen@gmail.com>
2021-05-22 18:25:38 -0500

    debugging

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -8,14 +8,12 @@ class AuthService {
     return decode(this.getToken());
   }
 
-  // check if user's logged in
   loggedIn() {
     // Checks if there is a saved token and it's still valid
     const token = this.getToken();
     return !!token && !this.isTokenExpired(token); // handwaiving here
   }
 
-  // check if token is expired
   isTokenExpired(token) {
     try {
       const decoded = decode(token);
